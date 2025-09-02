20e5649e85a1a87273b307efd169b67e8162be6f
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2025-09-02 22:54:32 +0200

    fin

--- youcode/7.c
+++ youcode/7.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main ()
 {
+    char c;
+    printf ("write a caractere")  ;
+    scanf("%c",&c) ;
+    if (c >= 65 && c <= 90)
+
+    {
+        printf ("the caractere is majuscule") ;
+
+    } else  {
+        printf ("the caractere is miniscule") ;
+    }
     
+
 }
